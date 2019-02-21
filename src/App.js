c2d4b4f82871afe702973877d15c835dca69f577
Moneer <kiraalasmar@gmail.com>
2019-02-21 01:36:31 -0800

    added base components, began setting up state with Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
