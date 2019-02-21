c1d37b64015ca8f380f5f501bbe40b9fad7bd6af
Moneer <kiraalasmar@gmail.com>
2019-02-21 14:57:27 -0800

    completed Track component

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
+import Spinner from "../layout/Spinner";
+import Track from "./Track";
+
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          console.log(value);
-          return <h1>Tracks</h1>;
+          const { track_list } = value;
+          const { heading } = value;
+          if (!track_list || track_list.length === 0) {
+            return <Spinner />;
+          } else {
+            return (
+              <React.Fragment>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => (
+                    <Track track={item.track} key={item.track.track_id} />
+                  ))}
+                </div>
+              </React.Fragment>
+            );
+          }
         }}
       </Consumer>
     );
