ab9d367b49fcc3ca28419a68e0124434b1099e28
Moneer <kiraalasmar@gmail.com>
2019-02-21 11:42:58 -0800

    further logic completed for setting up context state management

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "./context";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </BrowserRouter>
+      <Provider>
+        <BrowserRouter>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
